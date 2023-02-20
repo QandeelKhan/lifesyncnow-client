2f3a6c041b605061fff1496a9f9c74c953a10fc8
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-02-20 18:46:37 +0500

    auto commit by py script

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,63 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Helmet } from "react-helmet";
 import "../components/css/home.css";
-import { Link, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
-import { getToken, removeToken } from "../redux/services/localStorageService";
-import { useDispatch } from "react-redux";
-import {
-    useGetLoggedUserQuery,
-    useLogoutUserMutation,
-} from "../redux/services/userAuthApi";
-// import { setUserInfo, unSetUserInfo } from "../redux/features/userSlice";
-import {
-    setEmail,
-    setFirstName,
-    setId,
-    setLastName,
-} from "../redux/features/authSlice";
 
 const Home = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-
-    // this hook gives data and some other features, this access_token will goto userAuthApi and we get response
-    // data of server in our "data" property in hook, then we use "data" wherever in ui's.
-    const { access_token, refresh_token } = getToken();
-
-    console.log(`Bearer ${access_token}`);
-
-    const { data, isSuccess } = useGetLoggedUserQuery<any>({
-        access_token: access_token,
-        refresh_token: refresh_token,
-    });
-    console.log("My user data", data);
-
-    const { firstName, lastName, email, id, loggedIn } = useSelector(
-        (state: RootState) => state.auth
-    );
-
-    // storing user profile data in state
-    useEffect(() => {
-        if (data && isSuccess) {
-            dispatch(setId(data.id));
-            dispatch(setFirstName(data.first_name));
-            dispatch(setLastName(data.last_name));
-            dispatch(setEmail(data.email));
-        }
-    }, [data, isSuccess, dispatch]);
-
-    // on logout, unset data in store,unset token in store.remove token from browser
-    const handleLogout = () => {
-        // dispatch(unSetUserInfo());
-        removeToken();
-        // navigate("/login");
-    };
-
-    // for logout
-
-    const LogoutButton = () => {};
     return (
         <>
             <Helmet>
@@ -81,22 +26,7 @@ const Home = () => {
             </Helmet>
             <section id="home">
                 <div className="home-container">
-                    <h1>
-                        i am home {firstName} {lastName}
-                    </h1>
-                    {id ? (
-                        <div>
-                            <button onClick={handleLogout}>LogOut</button>
-                        </div>
-                    ) : (
-                        <>
-                            <Link to={"/registration"}>Sign Up</Link>
-                            <br />
-                            <Link to={"/login"}>Login</Link>
-                            {console.log(loggedIn)}
-                            {console.log(data)}
-                        </>
-                    )}
+                    <h1>i am home</h1>
                 </div>
             </section>
         </>
