d3adadc2ea9d3eb0099eeea58da87ee5aae9b123
qandeel <com.qandeelhaider@gmail.com>
2023-03-08 18:03:15 +0500

    miner change

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
+                    
                     <Route path="/skin-care-tips" element={<SkinCareTips/>} />
                     <Route path="/food-and-nutrition" element={<FoodAndNutrition/>} />
                     <Route path="/holistic-treatment" element={<HolisticTreatment/>} />
