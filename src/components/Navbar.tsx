f79c5b6d041ac36593e79434da21b3311d8f6453
usama-mahmood <darindahp@gmail.com>
2023-03-09 21:56:16 +0500

    auto commit by py script

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,60 +3,122 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import "./css/navbar.css";
 import { Link } from "react-router-dom";
+// import SideBarToggle from "./SideBarToggle";
+import "../components/css/side-bar-toggle.css";
+import { SidebarData } from "./sideBarData";
 
 const Navbar = () => {
     const [menuActive, setMenuActive] = useState(false);
+    const [toggleBar, setToggleBar] = useState(false);
 
     const handleToggle = () => {
         setMenuActive(!menuActive);
     };
 
+    const handleToggleBar = () => {
+        setToggleBar(!toggleBar);
+    };
+
     return (
-        <nav className="navbar">
-            <div className="navbar-brand">
-                <i className="fa-solid fa-bars"></i>
-                <Link to="/">YOUR LOGO HERE</Link>
+        <>
+            <div
+                className={
+                    toggleBar
+                        ? "side-bar-main-container active"
+                        : "side-bar-main-container"
+                }
+            >
+                <div className="toggle-close-icon" onClick={handleToggleBar}>
+                    close
+                </div>
+                <div className="side-bar-menu">
+                    <ul>
+                        {SidebarData.map((item, index) => {
+                            return (
+                                <li key={index} className={item.cName}>
+                                    <a href={item.path}>{item.title}</a>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+                <div className="side-bar-menu">
+                    <ul>
+                        <li>
+                            <a href="/">WELL+GOOD PODCAST</a>
+                        </li>
+                        <li>
+                            <a href="/">WELL+GOOD PODCAST</a>
+                        </li>
+                        <li>
+                            <a href="/">WELL+GOOD PODCAST</a>
+                        </li>
+                        <li>
+                            <a href="/">WELL+GOOD PODCAST</a>
+                        </li>
+                        <li>
+                            <a href="/">WELL+GOOD PODCAST</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="side-bar-footer">
+                    <span>Become an Insider</span>
+                    <div className="email-box">
+                        <label>Enter Email Address</label>
+                        <input type="text" id="first_name" name="first_name" />
+                    </div>
+                    <a href="/">SIGN UP</a>
+                </div>
             </div>
-            <button
-                className={`navbar-toggle ${menuActive ? "active" : ""}`}
-                onClick={handleToggle}
-            >
-                {menuActive ? (
+            {/* actual navbar */}
+            <nav className="navbar">
+                <div className="navbar-brand">
+                    <div className="open-side-bar" onClick={handleToggleBar}>
+                        open
+                    </div>
+                    <Link to="/">YOUR LOGO HERE</Link>
+                </div>
+                <button
+                    className={`navbar-toggle ${menuActive ? "active" : ""}`}
+                    onClick={handleToggle}
+                >
+                    {menuActive ? (
+                        <FontAwesomeIcon icon={faTimes} />
+                    ) : (
+                        <FontAwesomeIcon icon={faBars} />
+                    )}
+                </button>
+                <ul className={`navbar-menu ${menuActive ? "active" : ""}`}>
+                    <li>
+                        <Link to="/">HOME</Link>
+                    </li>
+                    <li>
+                        <Link to="/skin-care-tips">SKIN-CARE TIPS</Link>
+                    </li>
+                    <li>
+                        <Link to="/food-and-nutrition">FOOD AND NUTRITION</Link>
+                    </li>
+                    <li>
+                        <Link to="holistic-treatment">HOLISTIC TREATMENT</Link>
+                    </li>
+                    <li>
+                        <Link to="/fitness-tips">FITNESS TIPS</Link>
+                    </li>
+                    <li>
+                        <Link to="/relationship-tips">RELATIONSHIP TIPS</Link>
+                    </li>
+                    <li>
+                        <Link to="/entertairment">ENTERTAIRMENT</Link>
+                    </li>
+                </ul>
+                <button
+                    className={`navbar-close ${menuActive ? "active" : ""}`}
+                    onClick={handleToggle}
+                >
                     <FontAwesomeIcon icon={faTimes} />
-                ) : (
-                    <FontAwesomeIcon icon={faBars} />
-                )}
-            </button>
-            <ul className={`navbar-menu ${menuActive ? "active" : ""}`}>
-                <li>
-                    <Link to="/">HOME</Link>
-                </li>
-                <li>
-                    <Link to="/skin-care-tips">SKIN-CARE TIPS</Link>
-                </li>
-                <li>
-                    <Link to="/food-and-nutrition">FOOD AND NUTRITION</Link>
-                </li>
-                <li>
-                    <Link to="holistic-treatment">HOLISTIC TREATMENT</Link>
-                </li>
-                <li>
-                    <Link to="/fitness-tips">FITNESS TIPS</Link>
-                </li>
-                <li>
-                    <Link to="/relationship-tips">RELATIONSHIP TIPS</Link>
-                </li>
-                <li>
-                    <Link to="/entertairment">ENTERTAIRMENT</Link>
-                </li>
-            </ul>
-            <button
-                className={`navbar-close ${menuActive ? "active" : ""}`}
-                onClick={handleToggle}
-            >
-                <FontAwesomeIcon icon={faTimes} />
-            </button>
-        </nav>
+                </button>
+            </nav>
+        </>
     );
 };
 
---
+++ src/components/sideBarData.js
@@ -0,0 +1,86 @@
+import react from 'react';
+
+export const SidebarData = [
+    {
+        title: 'SKIN-CARE TIPS',
+        cName: 'anchor',
+        path: '/'
+    },
+    {
+        title: 'FOOD AND NUTRITION',
+        cName: 'anchor',
+        path: '/'
+    },
+    {
+        title: 'HOLISTIC TREATMENT',
+        cName: 'anchor',
+        path: '/'
+    },
+    {
+        title: 'FITNESS TIPS',
+        cName: 'anchor',
+        path: '/'
+    },
+    {
+        title: 'RELATIONSHIP TIPS',
+        cName: 'anchor',
+        path: '/'
+    },
+    {
+        title: 'SHOPPING',
+        cName: 'anchor',
+        path: '/'
+    },
+    {
+        title: 'ALL TOPICS',
+        cName: 'anchor',
+        path: '/'
+    },
+    {
+        title: 'RELATIONSHIP TIPS',
+        cName: 'anchor',
+        path: '/'
+    },
+]
+export const SidebarSecondData = [
+    {
+        title: 'SKIN-CARE TIPS',
+        cName: 'anchor',
+        path: '/'
+    },
+    {
+        title: 'FOOD AND NUTRITION',
+        cName: 'anchor',
+        path: '/'
+    },
+    {
+        title: 'HOLISTIC TREATMENT',
+        cName: 'anchor',
+        path: '/'
+    },
+    {
+        title: 'FITNESS TIPS',
+        cName: 'anchor',
+        path: '/'
+    },
+    {
+        title: 'RELATIONSHIP TIPS',
+        cName: 'anchor',
+        path: '/'
+    },
+    {
+        title: 'SHOPPING',
+        cName: 'anchor',
+        path: '/'
+    },
+    {
+        title: 'ALL TOPICS',
+        cName: 'anchor',
+        path: '/'
+    },
+    {
+        title: 'RELATIONSHIP TIPS',
+        cName: 'anchor',
+        path: '/'
+    },
+]
