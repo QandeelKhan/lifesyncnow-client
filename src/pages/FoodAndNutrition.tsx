59e0ec580b15c63e1ce00b4ec8b7ffc80a00035c
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-03-09 17:54:52 +0500

    auto commit by py script

--- src/pages/FoodAndNutrition.tsx
+++ src/pages/FoodAndNutrition.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import BlogpostCard from '../components/BlogpostCard'
-import '../components/css/foodAndNutrition.css'
+import React from "react";
+import BlogpostCard from "../components/BlogpostCard";
+import "../components/css/foodAndNutrition.css";
 const FoodAndNutrition = () => {
-  return (
-    <>
-    <h2> Food And Nutrition</h2>
-    <BlogpostCard/>
-    </>
-  )
-}
+    return (
+        <>
+            <h2> Food And Nutritions</h2>
 
-export default FoodAndNutrition
+            <BlogpostCard />
+        </>
+    );
+};
+
+export default FoodAndNutrition;
