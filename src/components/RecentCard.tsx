81484bc6d5fd272eb1e2019e6cd3b52a6c148fc9
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-03-10 19:21:13 +0500

    auto commit by py script

--- src/components/RecentCard.tsx
+++ src/components/RecentCard.tsx
@@ -1,27 +1,35 @@
-import React from 'react'
-import { recentData } from '../data'
-import './css/recentCard.css'
-import SectionHeading from './SectionHeading'
+import React from "react";
+import { recentData } from "../data";
+import "./css/recentCard.css";
+import SectionHeading from "./SectionHeading";
 const RecentCard = () => {
-  return (
-    <>
-    <div className='recent-section'>
-    <SectionHeading heading="Most Recent"/>
-  <div className='recent-container'>
-  {recentData.map((recent)=>(
-  <div className="recent-card" key={recent.id}>
-  <img src={recent.img} className="recent-img" alt="Card image"/>
-  <div className="recent-body">
-    <h3 className="recent-title">{recent.heading}</h3>
-    <p className="recent-author"> By {recent.author}</p>
-  </div>
-  </div>
-   ))}
-  </div>
- 
-  </div>
-    </>
-  )
-}
+    return (
+        <>
+            <div className="recent-section">
+                <SectionHeading heading="Most Recent" />
+                <div className="recent-container">
+                    {recentData.map((recent) => (
+                        <div className="recent-card" key={recent.id}>
+                            <img
+                                src={recent.img}
+                                className="recent-img"
+                                alt="Card image"
+                            />
+                            <div className="recent-body">
+                                <h3 className="recent-title">
+                                    {recent.heading}
+                                </h3>
+                                <p className="recent-author">
+                                    {" "}
+                                    By {recent.author}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+};
 
-export default RecentCard
+export default RecentCard;
