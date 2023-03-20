01b8310cd9dfc6aa691d0e419557d746246d9f4e
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-03-20 20:43:36 +0500

    auto commit by py script

--- src/pages/Advertise.tsx
+++ src/pages/Advertise.tsx
@@ -19,7 +19,23 @@ const Advertise = () => {
                     levels to craft a proposal <br /> + How you will measure
                     success (i.e. KPIs) <br /> + Optional: how you found us?
                 </p>
+                <br />
             </div>
+            {/* <div className="advertise-page-content">
+                <p>
+                    Please contact us at advertising@wellandgood.com to learn
+                    more about our advertising opportunities.
+                </p>
+                <p>
+                    + Don’t forget to include: <br />
+                    + Your name, email and phone <br /> + Company name <br /> +
+                    Business category/field <br /> + Timing of your potential ad
+                    campaign (i.e. – Sept – Nov) <br /> + High and low budget
+                    levels to craft a proposal <br /> + How you will measure
+                    success (i.e. KPIs) <br /> + Optional: how you found us?
+                </p>
+                <br />
+            </div> */}
         </PageTemplate>
     );
 };
