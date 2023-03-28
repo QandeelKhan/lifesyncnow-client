d74e83adc77443ed7c0e37bce6521016f5c3d3a3
usama-mahmood <darindahp@gmail.com>
2023-03-28 16:41:49 +0500

    auto commit by py script

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -22,6 +22,8 @@ const About = () => {
                     </span>
                 </div>
             </div>
+
+            {/* OUR PURPOSE SECTION STARTS HERE */}
             <div className="our-purpose-main-container">
                 <div className="our-purpose-content">
                     <div className="text-content">
@@ -87,6 +89,8 @@ const About = () => {
                     </div>
                 </div>
             </div>
+
+            {/* THIRD SECTION STARTS HERE */}
             <div className="main-third-container">
                 <div className="first-in-main">
                     <div className="left-text">
@@ -133,7 +137,7 @@ const About = () => {
                         </div>
                         <div className="para">
                             {" "}
-                            <h1>Our 15%% Pledge</h1>
+                            <h1>Our 15% Pledge</h1>
                             <span>
                                 We pledge that a minimum of 15% of our shopping
                                 content will feature brands with founders,
@@ -142,6 +146,119 @@ const About = () => {
                             </span>
                         </div>
                     </div>
+                </div>
+            </div>
+
+            {/* WE BELIEVE SECTION STARTS HERE */}
+            <div className="we-believe-main-container">
+                <h1>We Believe</h1>
+                <div className="points-main-container">
+                    <div className="left-points-container">
+                        <div className="point">
+                            <h3>1</h3>
+                            <span>
+                                Well-being is a right for all, and we have a
+                                responsibility to challenge the systems that
+                                prevent people from living a well life.
+                            </span>
+                        </div>
+                        <div className="point">
+                            <h3>2</h3>
+                            <span>
+                                In journalism and a research-backed approach.
+                            </span>
+                        </div>
+                        <div className="point">
+                            <h3>3</h3>
+                            <span>
+                                That lived experience is also valid,
+                                particularly for marginalized communities.
+                            </span>
+                        </div>
+                        <div className="point">
+                            <h3>4</h3>
+                            <span>
+                                Wellness is deeply personal and ever evolving:
+                                what works at 27 may not at 52 and beyond—we’re
+                                committed to meeting you wherever you are.
+                            </span>
+                        </div>
+                        <div className="point">
+                            <h3>5</h3>
+                            <span>
+                                That people's relationships with health, health
+                                care, and wellness are impacted differently by
+                                their race and ethnicity, socioeconomics,
+                                gender, sexuality, and ability. As such, we
+                                strive to make our content relevant and
+                                accessible to as many different kinds of people
+                                as possible.
+                            </span>
+                        </div>
+                        <div className="point">
+                            <h3>6</h3>
+                            <span>
+                                We can not truly be well if the planet is not.
+                            </span>
+                        </div>
+                    </div>
+                    <div className="right-points-container">
+                        <div className="point">
+                            <h3>7</h3>
+                            <span>
+                                Community care is self care, and we’ll continue
+                                to foster ours in all areas, with acknowledgment
+                                of when we fall short.
+                            </span>
+                        </div>
+                        <div className="point">
+                            <h3>8</h3>
+                            <span>
+                                Mental health is just as important as physical
+                                health.
+                            </span>
+                        </div>
+                        <div className="point">
+                            <h3>9</h3>
+                            <span>
+                                Wellness is always changing, and trend
+                                forecasting is core to our DNA.
+                            </span>
+                        </div>
+                        <div className="point">
+                            <h3>10</h3>
+                            <span>
+                                In a holistic approach to wellness, and that we
+                                have a duty to acknowledge and amplify the
+                                cultural roots of practices and rituals that we
+                                cover.
+                            </span>
+                        </div>
+                        <div className="point">
+                            <h3>11</h3>
+                            <span>
+                                Access to healthy ingredients and the
+                                opportunity to attend fitness classes and other
+                                wellness experiences is a privilege. We aim to
+                                open doors and provide accessible resources and
+                                alternatives for all.
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* OUR TEAM SECTION STARTS HERE */}
+            <div className="our-team-main-container">
+                <div className="our-team-left">
+                    <h1>OUR TEAM</h1>
+                    <span>
+                        Who's who on the content and growth team at Well+Good
+                    </span>
+                    <a href="/">SEE THE MASTHEAD</a>
+                </div>
+                <div className="our-team-right">
+                    <img src="our-team.webp" alt="responsive" />
                 </div>
             </div>
         </>
