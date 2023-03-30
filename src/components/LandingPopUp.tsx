695b66118fbcb9c9108c7c6f087ccc66fe230ab0
hasnain011 <chhasnain063@gmail.com>
2023-03-30 16:59:52 +0500

    auto commit by py script

--- src/components/LandingPopUp.tsx
+++ src/components/LandingPopUp.tsx
@@ -4,24 +4,33 @@ const LandingPopUp = () => {
     return (
         <div className="pop-up-main">
             <div className="pop-up">
-                <div className="content">
-                    <h3>Subscribe to Well+Good</h3>
-                    <div className="desc">
-                        <p>
-                            Get your daily dose of wellness delivered straight
-                            to your inbox.{" "}
-                        </p>
+                <div className="content-container">
+                    <div className="close">
+                        {" "}
+                        <i className="fa-solid fa-xmark"></i>
                     </div>
+                    <div className="content">
+                        <h3>Subscribe to Well+Good</h3>
+                        <div className="desc">
+                            <p>
+                                Get your daily dose of wellness delivered
+                                straight to your inbox.{" "}
+                            </p>
+                        </div>
 
-                    <form className="form">
-                        <input type="text" placeholder="example@gmail.com" />
-                        <button className="normal">Sign Up</button>
-                    </form>
-                    <div className="bottom-line">
-                        <span>Need Help?</span>
-                        <a href="">
-                            <span>Contact Support</span>
-                        </a>
+                        <form className="form">
+                            <input
+                                type="text"
+                                placeholder="example@gmail.com"
+                            />
+                            <button className="normal">Sign Up</button>
+                        </form>
+                        <div className="bottom-line">
+                            <span>Need Help?</span>
+                            <a href="">
+                                <span>Contact Support</span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
